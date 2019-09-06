295a1af6bc0e6c3ba932d5ec9e3f8f843c4c60a7
Piotr <ptrysinski@gmail.com>
2019-09-06 15:28:00 +0200

    showing a result

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
-function rollDice(){
+var result = 0;
+
+function rollDice() {
     var rand = Math.floor(Math.random() * 6) + 1;
+    result = result + rand;
     document.getElementById('dice_display').innerHTML = 'Wynik rzutu: ' + rand;
+    if (result === 12) {
+        document.getElementById('result_display').innerHTML = 'Trafiasz na pole specjalne nr 12, koniec gry';
+    }
+    else if (result === 19) {
+        result = 11;
+        document.getElementById('result_display').innerHTML = 'Trafiasz na pole specjalne nr 19, cofasz się na pole nr 11';
+    }
+    else if (result === 20) {
+        document.getElementById('result_display').innerHTML = 'Brawo, wygrałeś, dotarłeś na pole nr 20';
+    }
+    else if (result > 20) {
+        result = 20 - (rand - (20 - (result - rand)));
+        document.getElementById('result_display').innerHTML = 'Twoje pole: ' + result;
+        if (result === 19) {
+            result = 11;
+            document.getElementById('result_display').innerHTML = 'Trafiasz na pole specjalne nr 19, cofasz się na pole nr 11';
+        }
+    }
+    else{
+        document.getElementById('result_display').innerHTML = 'Twoje pole: ' + result;
+    }
 }
